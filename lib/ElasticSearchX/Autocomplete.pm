7831d867e9070ec826c3dc47b562ef4a48d7af09
Clinton Gormley <clinton@iannounce.co.uk>
2011-05-05 20:41:03 +0200

    Released version 0.02

--- lib/ElasticSearchX/Autocomplete.pm
+++ lib/ElasticSearchX/Autocomplete.pm
@@ -10,7 +10,7 @@ use ElasticSearchX::Autocomplete::Type();
 use JSON();
 
 our $JSON    = JSON->new()->utf8(1);
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 __PACKAGE__->_create_accessors(
     ['cache'],
@@ -82,7 +82,7 @@ and geolocation
 
 =head1 VERSION
 
-Version 0.01 - alpha
+Version 0.02 - alpha
 
 =head1 DESCRIPTION
 
